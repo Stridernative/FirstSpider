65e1c00c9bd7b2a5cd42b8ecee35240649dcbfe9
StriderNative <GabeCreaCS@gmail.com>
2022-01-25 10:35:13 -0800

    Updated For Loop to refine the search of crawler.
    Decision to remake project

--- link_finder.py
+++ link_finder.py
@@ -1,5 +1,6 @@
 from html.parser import HTMLParser
 from urllib import parse
+from bs4 import BeautifulSoup
 
 
 class LinkFinder(HTMLParser):
@@ -12,11 +13,25 @@ class LinkFinder(HTMLParser):
 
     # When we call HTMLParser feed() this function is called when it encounters an opening tag <a>
     def handle_starttag(self, tag, attrs):
+       # url = 'https://www.apple.com/retail/storelist/' + str(page)
+        #source_code = requests.get(url)
+       # if tag == ('a', {'class': 'data-store-number'}):
+        isStore = False
+        storeList = []
         if tag == 'a':
             for (attribute, value) in attrs:
-                if attribute == 'href':
+                if attribute == 'data-store-number':
+                   # print(attrs)
+                    isStore = True
+                if attribute == 'href' and isStore:
                     url = parse.urljoin(self.base_url, value)
                     self.links.add(url)
+                   # print(value)
+                    storeList.append('apple.com' + value)
+                    isStore = False
+
+            for store in storeList:
+                print(store)
 
     def page_links(self):
         return self.links
